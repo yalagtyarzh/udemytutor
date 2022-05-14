0ec1c9be61a80c5f8e6cdf09c9a1993b1c25e47e
Dmitry Shtanko <lempionnewliving@gmail.com>
2022-05-14 19:17:20 +0300

    Glob go templates

--- template/main.go
+++ template/main.go
@@ -1,38 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"html/template"
 	"log"
 	"os"
-	"strings"
 )
 
+var tmpl *template.Template
+
 func main() {
-	name := os.Args[1]
-	fmt.Println(os.Args[0])
-	fmt.Println(os.Args[1])
-	str := `
-	<!DOCTYPE html>
-	<html lang="ru">
-	<head>
-	<meta charset="UTF-8">
-	<title>Hello World!</title>
-	</head>
-	<body>
-	<h1>` + name + `</h1>
-	</body>
-	</html>
-	`
-
-	nf, err := os.Create("index.html")
+	tmpl = template.Must(template.ParseGlob("templates/*.tmpl"))
+	err := tmpl.ExecuteTemplate(os.Stdout, "tmpl.tmpl", nil)
 	if err != nil {
-		log.Fatal("error creating file", err)
-	}
-	defer nf.Close()
-
-	_, err = io.Copy(nf, strings.NewReader(str))
-	if err != nil {
-		log.Fatal("error copying data", err)
+		log.Fatalln(err)
 	}
 }
