a196a3abcf515fad5196f8bff0aae4c32065b56f
Dmitry Shtanko <lempionnewliving@gmail.com>
2022-05-15 17:05:13 +0300

    xded

--- template/main.go
+++ template/main.go
@@ -6,11 +6,43 @@ import (
 	"os"
 )
 
+type course struct {
+	Number, Name, Units string
+}
+
+type semester struct {
+	Term    string
+	Courses []course
+}
+
+type year struct {
+	Fall, Spring, Summer semester
+}
+
 var tmpl *template.Template
 
 func main() {
 	tmpl := template.Must(template.ParseGlob("./templates/*.tmpl"))
-	err := tmpl.ExecuteTemplate(os.Stdout, "index.tmpl", "xd")
+	y := year{
+		Fall: semester{
+			Term: "Fall",
+			Courses: []course{
+				course{"CSCI-40", "Introduction to Programming in Go", "4"},
+				course{"CSCI-130", "Introduction to Web Programming in Go", "4"},
+				course{"CSCI-140", "Mobile Apps Using Go", "4"},
+			},
+		},
+		Spring: semester{
+			Term: "Spring",
+			Courses: []course{
+				course{"CSCI-50", "Advanced Go", "5"},
+				course{"CSCI-190", "Advanced Web Programming with Go", "5"},
+				course{"CSCI-191", "Advanced Mobile Apps With Go", "5"},
+			},
+		},
+	}
+
+	err := tmpl.Execute(os.Stdout, y)
 	if err != nil {
 		log.Fatalln(err)
 	}
