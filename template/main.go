871b7179ff57d4967d6f5ae02904241de2e4d673
Dmitry Shtanko <lempionnewliving@gmail.com>
2022-05-15 17:23:31 +0300

    hands-oned once

--- template/main.go
+++ template/main.go
@@ -6,33 +6,70 @@ import (
 	"os"
 )
 
-type person struct {
-	Name string
-	Age  int
+type course struct {
+	Number string
+	Name   string
+	Units  string
 }
 
-func (p person) SomeProcessing() int {
-	return 7
+type semester struct {
+	Term    string
+	Courses []course
 }
 
-func (p person) AgeDbl() int {
-	return p.Age * 2
-}
-
-func (p person) TakesArg(x int) int {
-	return x * 2
+type year struct {
+	AcaYear string
+	Fall    semester
+	Spring  semester
+	Summer  semester
 }
 
 var tmpl *template.Template
 
 func main() {
 	tmpl := template.Must(template.ParseGlob("./templates/*.tmpl"))
-	p := person{
-		Name: "Iam Fleming",
-		Age:  56,
+	years := []year{
+		year{
+			AcaYear: "2020-2021",
+			Fall: semester{
+				Term: "Fall",
+				Courses: []course{
+					course{"CSCI-40", "Introduction to Programming in Go", "4"},
+					course{"CSCI-130", "Introduction to Web Programming with Go", "4"},
+					course{"CSCI-140", "Mobile Apps Using Go", "4"},
+				},
+			},
+			Spring: semester{
+				Term: "Spring",
+				Courses: []course{
+					course{"CSCI-50", "Advanced Go", "5"},
+					course{"CSCI-190", "Advanced Web Programming with Go", "5"},
+					course{"CSCI-191", "Advanced Mobile Apps With Go", "5"},
+				},
+			},
+		},
+		year{
+			AcaYear: "2021-2022",
+			Fall: semester{
+				Term: "Fall",
+				Courses: []course{
+					course{"CSCI-40", "Introduction to Programming in Go", "4"},
+					course{"CSCI-130", "Introduction to Web Programming with Go", "4"},
+					course{"CSCI-140", "Mobile Apps Using Go", "4"},
+				},
+			},
+			Spring: semester{
+				Term: "Spring",
+				Courses: []course{
+					course{"CSCI-50", "Advanced Go", "5"},
+					course{"CSCI-190", "Advanced Web Programming with Go", "5"},
+					course{"CSCI-191", "Advanced Mobile Apps With Go", "5"},
+				},
+			},
+		},
 	}
 
-	err := tmpl.Execute(os.Stdout, p)
+	err := tmpl.Execute(os.Stdout, years)
 	if err != nil {
 		log.Fatalln(err)
 	}
