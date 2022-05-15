22e72e35aee3e2f256a4915341f5e85c0e2ee27d
Dmitry Shtanko <lempionnewliving@gmail.com>
2022-05-15 16:38:11 +0300

    eq/not checked

--- template/main.go
+++ template/main.go
@@ -6,38 +6,20 @@ import (
 	"os"
 )
 
-type user struct {
-	Name  string
-	Motto string
-	Admin bool
-}
-
 var tmpl *template.Template
 
 func main() {
 	tmpl := template.Must(template.ParseGlob("./templates/*.tmpl"))
 
-	u1 := user{
-		Name:  "Buddha",
-		Motto: "Be the change",
-		Admin: false,
+	g1 := struct {
+		Score1 int
+		Score2 int
+	}{
+		7,
+		9,
 	}
 
-	u2 := user{
-		Name:  "Gandhi",
-		Motto: "Be the change",
-		Admin: true,
-	}
-
-	u3 := user{
-		Name:  "",
-		Motto: "Nobody",
-		Admin: true,
-	}
-
-	users := []user{u1, u2, u3}
-
-	err := tmpl.Execute(os.Stdout, users)
+	err := tmpl.Execute(os.Stdout, g1)
 	if err != nil {
 		log.Fatalln(err)
 	}
