c5f1d1b155afc095969cdb2b22583ac24121af29
Dmitry Shtanko <lempionnewliving@gmail.com>
2022-05-16 22:11:37 +0300

    ram memory tried

--- tcpserv/main.go
+++ tcpserv/main.go
@@ -1,16 +1,66 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net"
+	"strings"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8080")
+	li, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		panic(err)
 	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
 	defer conn.Close()
 
-	fmt.Fprintln(conn, "I dialed you.")
+	io.WriteString(conn, "\nIN-MEMORY DATABASE\n\n"+
+		"USE:\n"+
+		"SET key value\n"+
+		"GET key\n"+
+		"DEL key\n\n"+
+		"EXAMPLE:\n"+
+		"SET fav chocolate\n"+
+		"GET fav\n\n\n")
+
+	data := make(map[string]string)
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fs := strings.Fields(ln)
+
+		switch fs[0] {
+		case "GET":
+			k := fs[1]
+			v := data[k]
+			fmt.Fprintf(conn, "%s\n", v)
+
+		case "SET":
+			if len(fs) != 3 {
+				fmt.Fprintln(conn, "EXPECTED VALUE")
+				continue
+			}
+			k := fs[1]
+			v := fs[2]
+			data[k] = v
+		case "DEL":
+			k := fs[1]
+			delete(data, k)
+		default:
+			fmt.Fprintln(conn, "INVALID COMMAND")
+		}
+	}
 }
