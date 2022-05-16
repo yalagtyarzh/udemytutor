e4c2262be6150aef3299d538951a698c8f957b8c
Dmitry Shtanko <lempionnewliving@gmail.com>
2022-05-16 23:10:35 +0300

    ListenAndServed

--- tcpserv/main.go
+++ tcpserv/main.go
@@ -1,60 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"net"
-	"strings"
+	"net/http"
 )
 
+type hotdog int
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Any code you want in this func")
+}
+
 func main() {
-	li, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		panic(err)
-	}
-	defer li.Close()
-
-	for {
-		conn, err := li.Accept()
-		if err != nil {
-			panic(err)
-		}
-		go handle(conn)
-	}
-}
-
-func handle(conn net.Conn) {
-	defer conn.Close()
-
-	request(conn)
-
-	respond(conn)
-}
-
-func request(conn net.Conn) {
-	i := 0
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		ln := scanner.Text()
-		fmt.Println(ln)
-		if i == 1 {
-			m := strings.Fields(ln)[1]
-			fmt.Println("***URL", m)
-		}
-
-		if ln == "" {
-			break
-		}
-		i++
-	}
-}
-
-func respond(conn net.Conn) {
-	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8><title></title></head><body><strong>Hello world</strong></body></html>`
-
-	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
-	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
-	fmt.Fprint(conn, "Content-Type: text/html\r\n")
-	fmt.Fprint(conn, "\r\n")
-	fmt.Fprint(conn, body)
+	var d hotdog
+	http.ListenAndServe(":8080", d)
 }
