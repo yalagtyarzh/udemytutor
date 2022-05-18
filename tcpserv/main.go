d767a8e38beee37b2b1457e2e3e9aef4a6e20092
Dmitry Shtanko <lempionnewliving@gmail.com>
2022-05-18 21:31:44 +0300

    url valued

--- tcpserv/main.go
+++ tcpserv/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"text/template"
 )
 
 type hotdog int
 
-func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Any code you want in this func")
+func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tmpl.ExecuteTemplate(w, "index.gohtml", req.Form)
 }
 
+var tmpl *template.Template
+
 func main() {
+	tmpl = template.Must(template.ParseGlob("*.gohtml"))
+
 	var d hotdog
 	http.ListenAndServe(":8080", d)
 }
