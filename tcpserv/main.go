591219107aeeda2dd20f7ce9d3bafd8ea95d7ecb
Dmitry Shtanko <lempionnewliving@gmail.com>
2022-05-19 13:08:57 +0300

    hands-oned once

--- tcpserv/main.go
+++ tcpserv/main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"text/template"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 func dog(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "dog dog doggy")
 }
 
-func cat(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "cat cat cat")
+func index(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "home")
+}
+
+func me(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	io.WriteString(w, "Alister")
 }
 
 var tmpl *template.Template
@@ -23,63 +24,9 @@ var tmpl *template.Template
 func main() {
 	tmpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 
-	mux := httprouter.New()
-	mux.GET("/", index)
-	mux.GET("/about", about)
-	mux.GET("/contact", contact)
-	mux.GET("/apply", applyProcess)
-	mux.POST("/apply", applyProcess)
-	mux.GET("/user/:name", user)
-	mux.GET("/blog/:category/:article", blogRead)
-	mux.POST("/blog/:category/:article", blogWrite)
-	http.ListenAndServe(":8080", mux)
-}
+	http.HandleFunc("/", index)
+	http.HandleFunc("/dog/", dog)
+	http.HandleFunc("/me/", me)
 
-func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	err := tmpl.ExecuteTemplate(w, "index.gohtml", nil)
-	HandleError(w, err)
-}
-
-func about(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	err := tmpl.ExecuteTemplate(w, "about.gohtml", nil)
-	HandleError(w, err)
-}
-
-func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	err := tmpl.ExecuteTemplate(w, "apply.gohtml", nil)
-	HandleError(w, err)
-}
-
-func applyProcess(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	err := tmpl.ExecuteTemplate(w, "applyProcess.gohtml", nil)
-	HandleError(w, err)
-}
-
-func user(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "USER %s!\n", ps.ByName("name"))
-}
-
-func blogRead(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "READ CATEGORY, %s!\n", ps.ByName("category"))
-	fmt.Fprintf(w, "READ ARTICLE, %s!\n", ps.ByName("article"))
-}
-
-func blogWrite(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "WRITE CATEGORY, %s!\n", ps.ByName("category"))
-	fmt.Fprintf(w, "WRITE ARTICLE, %s!\n", ps.ByName("article"))
-}
-
-func update(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "UPDATE, %s!\n", ps.ByName("name"))
-}
-
-func trailing(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "TRAILING, %s!\n", ps.ByName("name"))
-}
-
-func HandleError(w http.ResponseWriter, e error) {
-	if e != nil {
-		http.Error(w, e.Error(), http.StatusInternalServerError)
-		log.Println(e)
-	}
+	http.ListenAndServe(":8080", nil)
 }
