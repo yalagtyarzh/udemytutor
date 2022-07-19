fc5fd8ae2565e51f43f2ecb9979cc9904b624c84
agrael <lempionnewliving@gmail.com>
2022-07-19 12:57:16 +0300

    hands-oned thrice and quadruple

--- fileserv/main.go
+++ fileserv/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var tmpl *template.Template
+
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
+	tmpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+
+	http.HandleFunc("/", dogs)
+	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("public"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func dogs(w http.ResponseWriter, r *http.Request) {
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatalln("template didn't execute:", err)
+	}
 }
