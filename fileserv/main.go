d7929ee4272f344668e5b2f417ebee69d386d81f
agrael <lempionnewliving@gmail.com>
2022-07-19 12:38:52 +0300

    hands-oned twice

--- fileserv/main.go
+++ fileserv/main.go
@@ -1,31 +1,10 @@
 package main
 
 import (
-	"html/template"
-	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", foo)
-	http.HandleFunc("/dog/", dog)
-	http.HandleFunc("/dog.jpg", dogPic)
-	http.ListenAndServe(":8080", nil)
-}
-
-func foo(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "foo ran")
-}
-
-func dog(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("dog.gohtml")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	tmpl.ExecuteTemplate(w, "dog.gohtml", nil)
-}
-
-func dogPic(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "dog.jpg")
+	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
 }
